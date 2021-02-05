a1f479e07cea8f35df8e49d0ede60af409f7c0f6
Tephi Godfrey <tephilla.allanroy@m.ecsfin.com>
2021-02-04 22:25:18 -0500

    Game Page added

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Layout, { Content, Header, Button } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
 
 const GamePage = () => {
-  return <div>Game Page</div>;
+  return (
+    <div style={{ color: "#008080" }}>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header>
+          <Button type="primary" icon={<LogoutOutlined />} />
+        </Header>
+        <Content>
+          <div>Game is displayed here</div>
+        </Content>
+      </Layout>
+    </div>
+  );
 };
 
 export default GamePage;
