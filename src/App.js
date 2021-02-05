f393aae9e276be298324f3cbc2dd94a03bc20360
Tephi Godfrey <tephilla.allanroy@m.ecsfin.com>
2021-02-04 22:36:46 -0500

    Adding Logout button to GamePage and basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import LoginPage from "./components/LoginPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GamePage from "./components/GamePage";
 
 function App() {
   return (
     <div className="app-content">
-      <LoginPage />
+      <Router>
+        <Switch>
+          <Route path="/" component={LoginPage} exact />
+          <Route path="/game-page" component={GamePage} exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,16 +1,28 @@
 import React from "react";
-import Layout, { Content, Header, Button } from "antd";
+import { Layout, Button } from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
+const { Header, Content } = Layout;
 const GamePage = () => {
+  const history = useHistory();
   return (
     <div style={{ color: "#008080" }}>
       <Layout style={{ minHeight: "100vh" }}>
         <Header>
-          <Button type="primary" icon={<LogoutOutlined />} />
+          <div style={{ float: "right" }}>
+            <Button
+              type="primary"
+              icon={<LogoutOutlined />}
+              onClick={() => {
+                sessionStorage.removeItem("token");
+                history.push("/");
+              }}
+            />
+          </div>
         </Header>
         <Content>
-          <div>Game is displayed here</div>
+          <div className="center-content">Game is displayed here</div>
         </Content>
       </Layout>
     </div>
