86e281592acb377de296efd35be238a61e2fa21c
Tephi Godfrey <tephilla.allanroy@m.ecsfin.com>
2021-02-04 22:14:55 -0500

    Login Form added

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import "../styles/LoginPage.css";
 
+/** Displays the login screen of the app */
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <div className="center-content">
+      <h2> Please Enter your email to Login! </h2>
+      <Form {...layout} name="login" onFinish={onFinish}>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default LoginPage;
