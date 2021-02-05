9be4f4ad982b5dab98a24a3b69d74f0cb1fae9b1
Tephi Godfrey <tephilla.allanroy@m.ecsfin.com>
2021-02-04 23:45:00 -0500

    QueryClient Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
 import LoginPage from "./components/LoginPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GamePage from "./components/GamePage";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // query options
+      refetchOnWindowFocus: false,
+      staleTime: 0,
+      retry: false,
+    },
+    mutations: {
+      // mutation options
+    },
+  },
+});
 function App() {
   return (
     <div className="app-content">
-      <Router>
-        <Switch>
-          <Route path="/" component={LoginPage} exact />
-          <Route path="/game-page" component={GamePage} exact />
-        </Switch>
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Router>
+          <Switch>
+            <Route path="/" component={LoginPage} exact />
+            <Route path="/game-page" component={GamePage} exact />
+          </Switch>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
